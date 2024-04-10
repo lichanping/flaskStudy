95319351cafdd90c294935b8edf4607c12ec82c7
shirley <shirley.li@lego.com>
2024-04-10 23:28:44 +0800

    3

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import json
 import os
 import re
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 # Configure Flask to serve static files from the 'static' directory
@@ -22,34 +23,76 @@ class TxtReader:
         words = []
         pattern = re.compile(r'([a-zA-Z\'\s\-\.\/]+)\s*(.*)')
         with open(file_path, 'r', encoding='utf-8') as file:
-            for line in file:
+            for index, line in enumerate(file, start=1):  # Start index from 1
+                if index > limit:  # Break if the limit is reached
+                    break
                 match = pattern.match(line.strip())
                 if match:
                     english_word, translation = match.groups()
-                    words.append({"单词": english_word.strip(), "释意": translation})
+                    words.append({"索引": index, "单词": english_word.strip(), "释意": translation})
                 else:
                     print(f"Invalid format in line: {line.strip()}")
-                # Stop reading after reaching the limit
-                if len(words) >= limit:
-                    break
         return words
+
+    def move_words_to_new_file(self, selected_file, selected_words):
+        # Construct the path to the selected file
+        file_path = os.path.join(self.data_folder, selected_file)
+
+        # Construct the new file name
+        new_file_name = selected_file.replace('.txt', '_finish.txt')
+        new_file_path = os.path.join(self.data_folder, new_file_name)
+
+        # Move selected words to the new file
+        with open(file_path, 'r', encoding='utf-8') as file:
+            # Read the existing content
+            existing_content = file.readlines()
+
+        with open(new_file_path, 'a', encoding='utf-8') as new_file:
+            # Write selected words to the new file
+            for word in selected_words:
+                new_file.write(word+'\n')
+
+        # Update the original file by removing selected words
+        pattern = re.compile(r'([a-zA-Z\'\s\-\.\/]+)\s*(.*)')
+        with open(file_path, 'w', encoding='utf-8') as file:
+            # Write back the remaining content excluding selected words
+            for line in existing_content:
+                match = pattern.match(line.strip())
+                if match:
+                    english_word, translation = match.groups()
+                    if english_word.strip() not in selected_words:
+                        file.write(line)
+                else:
+                    print(f"Invalid format in line: {line.strip()}")
+
+        return new_file_name
 
 
 txt_reader = TxtReader()
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    file_name = request.args.get('file_name', 'words.txt')  # Default to 'words.txt' if no file selected
-    words = txt_reader.read_words_from_txt(file_name)
-    return render_template('index.html', words=words)
+    if request.method == 'GET':
+        file_name = request.args.get('file_name', 'words.txt')  # Default to 'words.txt' if no file selected
+        words = txt_reader.read_words_from_txt(file_name)
+        return render_template('index.html', words=words)
+    elif request.method == 'POST':
+        action = request.form['action']
+        if action == 'check':
+            selected_file = request.form.get('selected_file', 'words.txt')  # Default to 'words.txt' if no file selected
+            words = txt_reader.read_words_from_txt(selected_file)
+            return render_template('index.html', words=words)
+        elif action == 'learn':
+            selected_file = request.form['file_name']
+            selected_check_words = request.form.getlist('check_word')
+            new_file_name = txt_reader.move_words_to_new_file(selected_file, selected_check_words)
+            selected_check_words = request.form.getlist('selected_words')[0]
+            selected_check_words = json.loads(selected_check_words)
+            return render_template('index.html', words=selected_check_words)
+        elif action == 'resist_forgetting':
+            selected_check_words = request.form.getlist('check_word')
 
-
-@app.route('/refresh_word_list', methods=['GET'])
-def refresh_word_list():
-    selected_file = request.args.get('file_name', 'words.txt')
-    words = txt_reader.read_words_from_txt(selected_file)
-    return jsonify(words)
 
 
 if __name__ == '__main__':
