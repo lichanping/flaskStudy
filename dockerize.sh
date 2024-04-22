4b3cb5c7ad98643a9185f691d0f3d8aa5e2e03ff
Jeff Tian <jeff.tian@outlook.com>
2024-04-22 17:02:01 +0800

    cicd: pass docker username to dockerize shell

--- dockerize.sh
+++ dockerize.sh
@@ -1,9 +1,9 @@
-docker build -t jefftian/flask-study:"$1" .
+docker build -t "$2"/flask-study:"$1" .
 docker images
-docker run --network host -e CI=true -d -p 127.0.0.1:5000:5000 --name flask-study jefftian/flask-study:"$1"
+docker run --network host -e CI=true -d -p 127.0.0.1:5000:5000 --name flask-study "$2"/flask-study:"$1"
 docker ps | grep -q flask-study
 docker ps -aqf "name=flask-study$"
-docker push jefftian/flask-study:"$1"
+docker push "$2"/flask-study:"$1"
 docker logs $(docker ps -aqf name=flask-study$)
 curl localhost:5000 || docker logs $(docker ps -aqf name=flask-study$)
 docker kill flask-study || echo "flask-study killed"
