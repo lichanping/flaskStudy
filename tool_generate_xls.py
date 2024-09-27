b592d814886c35052e54c9c2bfe8cbee83c95a9a
cnshirli <lichanping@hotmail.com>
2024-09-27 10:38:22 +0800

    new

--- tool_generate_xls.py
+++ tool_generate_xls.py
@@ -60,6 +60,7 @@ class FrenchTTSProcessor:
             else:
                 excluded_voices = [
                     'Microsoft Server Speech Text to Speech Voice (fr-FR, EloiseNeural)',
+                    'Microsoft Server Speech Text to Speech Voice (fr-FR, VivienneMultilingualNeural)'
                 ]
                 # Choose voice from the voice library
                 all_voices = voices.find(Language="fr", Locale="fr-FR")  # For French voices
