c45fae672be1d8164ca846456e763f49a2a09c9d
cnshirli <lichanping@hotmail.com>
2024-10-02 21:35:37 +0800

    new

--- learnWords.js
+++ learnWords.js
@@ -15,7 +15,7 @@ class LearnWords {
             const response = await fetch(filePath);
             const text = await response.text();
             const data = [];
-            const pattern = /^([a-zA-ZéèêëîïùûüàâäôöçœÉ'’\s-\/\.\?\？]+)\s*(.*)$/;
+            const pattern = /^([a-zA-ZéèêëîïùûüàâäôöçœÉÇÀ'’\s-\/\.\?\？]+)\s*(.*)$/;
             const encounteredWords = new Set();
             text.split('\n').forEach(line => {
                 const match = line.trim().match(pattern);
