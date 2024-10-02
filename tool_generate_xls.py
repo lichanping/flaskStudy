1e08c99e573efa36996d63ccb6cb42fa18f9371a
lichanping <lichanping@users.noreply.github.com>
2024-10-02 13:36:07 +0000

    deploy: c45fae672be1d8164ca846456e763f49a2a09c9d

--- tool_generate_xls.py
+++ tool_generate_xls.py
@@ -135,7 +135,7 @@ class TxtToXLSX:
         duplicates = {}
         with open(file_path, 'r', encoding='utf-8') as file:
             for line in file:
-                match = re.match(r'([a-zA-ZéèêëîïùûüàâäôöçœÉ\'\s\-\.\/\?\？]+)\s*(.*)', line.strip())
+                match = re.match(r'([a-zA-ZéèêëîïùûüàâäôöçœÉÇÀ\'\s\-\.\/\?\？]+)\s*(.*)', line.strip())
                 if match:
                     english_word, translation = match.groups()
                     translation = re.sub(r'\s+', '', translation)
@@ -183,7 +183,7 @@ class TxtToXLSX:
         missing_sound_file = os.path.join(self.data_folder, "MissingSound.txt")  # Path to store missing sound words
         file_path = os.path.join(self.data_folder, file_name)
         data = []
-        pattern = re.compile(r'([a-zA-ZéèêëîïùûüàâäôöçœÉ\'\s\-\.\/\?\？]+)\s*(.*)')
+        pattern = re.compile(r'([a-zA-ZéèêëîïùûüàâäôöçœÉÇÀ\'\s\-\.\/\?\？]+)\s*(.*)')
         with open(file_path, 'r', encoding='utf-8') as file:
             for line in file:
                 match = pattern.match(line.strip())
