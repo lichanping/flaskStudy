33c0dc6f6f4a47078c95cdb5c6d7c97214bfb03b
lichanping@hotmail.com <lichanping@hotmail.com>
2024-12-13 22:50:22 +0800

    ext

--- tool_generate_xls.py
+++ tool_generate_xls.py
@@ -142,7 +142,7 @@ class TxtToXLSX:
                     english_word = english_word.strip()
 
                     if english_word.endswith(('adj.', 'adv.', 'n.', 'v.', 'phr.', 'vt.', 'prep.', 'vi.', 'det.',
-                                              'pron.', 'conj.', 'int.', 'aux.', 'auxv.', 'num.', "abbr.")):
+                                              'pron.', 'conj.', 'int.', 'aux.', 'auxv.', 'num.', "abbr.", "excl.")):
                         # If it does, move the part of speech to the translation
                         pos = english_word.split()[-1]  # Get the last part of the word as part of speech
                         english_word = english_word[
