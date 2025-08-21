f25c2a1e2f50af4a74220a7ca6c3e84219c7fa1d
lichanping@hotmail.com <lichanping@hotmail.com>
2025-08-21 22:57:38 +0800

    new

--- tool_generate_xls.py
+++ tool_generate_xls.py
@@ -343,7 +343,8 @@ class TestGenerateTool:
             '词库源/法语单词（持续更新中）.txt',
             '词库源/你好法语（持续更新中）.txt',
             '词库源/法语口语课（持续更新中）.txt',
-            '词库源/法语单词闪卡（持续更新中）.txt'
+            '词库源/法语单词闪卡（持续更新中）.txt',
+            '词库源/法语歌.txt',
         ]
         tool = TxtToXLSX()
         for file in files:
